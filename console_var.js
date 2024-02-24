6fe41bf67f86f610ffc404adaa2311a28f9ab32e
vinay balepur <vinay.balepur@viacom18.com>
2024-02-24 16:46:37 +0530

    first commit

---
+++ console_var.js
@@ -0,0 +1,32 @@
+console.log("Hello world")
+
+
+let a = 1
+console.log(typeof(a))
+
+let b = 'hello'
+console.log(typeof(b))
+
+let c = true
+console.log(typeof(c))
+
+let d = ''
+console.log(typeof(d))
+
+let e = null
+console.log(typeof(e))
+
+let arr = ["skoda", "ford"]
+console.log(typeof(arr))
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+console.log(typeof(person))
+
+let car
+console.log(typeof(car))
+
+let empty = ""
+console.log(typeof(empty))
+
+let date = new Date("22/02/2034")
+console.log(typeof(date))
